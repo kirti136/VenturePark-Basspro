8889a1275b4f23701d737a0ba4a7676305fb348d
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-03-29 11:52:29 +0530

    registration part done

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const { UserModel } = require("../Models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userRouter = Router();
 
@@ -15,28 +17,69 @@ userRouter.get("/", async (req, res) => {
 
 // Register User
 userRouter.post("/register", async (req, res) => {
-  const payload = req.body;
+  const { name, email, password } = req.body;
+
   try {
-    const user = new UserModel(payload);
-    await user.save();
-    res.status(200).send({ message: "User Registered Successfully" });
+    bcrypt.hash(password, 5, async (err, hash) => {
+      if (hash) {
+        const user = new UserModel({ name, email, password: hash });
+        await user.save();
+        res.status(200).send({ msg: "Registration successful" });
+      }
+    });
   } catch (error) {
-    res.status(400).send({ message: error.message });
+    res.status(400).send({ msg: "Registration Failed", error: error.message });
   }
 });
 
-// Login User 
+// Login User
 userRouter.post("/login", async (req, res) => {
-  // const { email, password } = req.body
+  const { email, password } = req.body;
+
   try {
-    const user = await UserModel.findOne(req.body);
+    const user = await UserModel.findOne({ email });
+
     if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          res.status(200).send({
+            msg: "Login successful",
+            token: jwt.sign({ userID: user._id }, "hello"),
+          });
+        } else {
+          res.status(400).send({ msg: "Wrong Password" });
+        }
+      });
     } else {
-      res.status(404).send({ message: "User not found" });
+      res.status(400).send({ msg: "Email Not Found" });
     }
-    // res.status(200).send("Login Successfully")
   } catch (error) {
-    res.status(400).send({ message: error.message });
+    res.status(400).send({ msg: "Login Failed", error: error.message });
+  }
+});
+
+// Update user
+userRouter.patch("/update/:userID", async (req, res) => {
+  const { userID } = req.params;
+  const payload = req.body;
+
+  try {
+    await UserModel.findByIdAndUpdate({ _id: userID }, payload);
+    res.status(200).send({ msg: "User Updated" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
+// Delete
+userRouter.delete("/delete/:userID", async (req, res) => {
+  const userID = req.params.userID;
+
+  try {
+    await UserModel.findByIdAndDelete({ _id: userID });
+    res.status(200).send({ msg: "User Deleted" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
   }
 });
 
